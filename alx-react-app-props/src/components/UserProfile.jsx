7c6d52600b64442311ae44744309d27af49f4d56
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-11-10 19:07:02 +0300

    Changes

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import {useContext} from 'react'
-import UserContext from '../UserContext'
+import UserContext from './UserContext'
 
 export default function UserProfile () {
     const usersData = useContext(UserContext)
