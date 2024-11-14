09e23232c3c85e68f014a06026e4f9165ea76667
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-11-14 22:42:45 +0300

    Change store name from useRecipeStore to recipeStore

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 // RecipeList component
-import useRecipeStore from './useRecipeStore';
+import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
     const recipes = useRecipeStore( state => state.recipes );
